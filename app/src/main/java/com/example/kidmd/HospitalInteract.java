ecd0e47515725b4b32c54e447d628676d13e17f6
ZPrints <48689811+ZPrints@users.noreply.github.com>
2022-02-26 17:19:22 -0800

    Interact w/ Hospital Room layout and entry
    - Added (possibly placeholder) Hospital room image to interact with
    -Changed base layout of entries to relative
    -Added Blood Pressure Monitor Entry

--- app/src/main/java/com/example/kidmd/HospitalInteract.java
+++ app/src/main/java/com/example/kidmd/HospitalInteract.java
@@ -12,7 +12,9 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
@@ -23,6 +25,7 @@ public class HospitalInteract extends AppCompatActivity {
     private AlertDialog dialog;
     private TextView bedDesc;
     private Button test;
+    private ImageButton bedButton, bpmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,26 +33,55 @@ public class HospitalInteract extends AppCompatActivity {
         setContentView(R.layout.activity_hospital_interact);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        test = (Button) findViewById(R.id.testButton);
-        test.setOnClickListener(this::onClick);
+        bedButton = (ImageButton) findViewById(R.id.bedButton);
+        bedButton.setOnClickListener(this::onClick);
+        bpmButton = (ImageButton) findViewById(R.id.bpmButton);
+        bpmButton.setOnClickListener(this::onClick);
 
     }
 
 
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.testButton:
+            case R.id.bedButton:
                 seeBed();
+                break;
+            case R.id.bpmButton:
+                seeBPM();
                 break;
         }
     }
 
     public void seeBed(){
+        //Create dialogue for bed
         dialogueBuilder = new AlertDialog.Builder(this);
         final View bedPopUpView = getLayoutInflater().inflate(R.layout.activity_hr_bed, null);
         dialogueBuilder.setView(bedPopUpView);
         dialog = dialogueBuilder.create();
+        //Set window parameters to wrap content
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
         dialog.show();
+        dialog.getWindow().setAttributes(lp);
+        //startActivity(new Intent(HospitalInteract.this, hrBed.class));;
+
+    }
+    public void seeBPM(){
+        //Create dialogue for bed
+        dialogueBuilder = new AlertDialog.Builder(this);
+        final View bedPopUpView = getLayoutInflater().inflate(R.layout.activity_hr_bpm, null);
+        dialogueBuilder.setView(bedPopUpView);
+        dialog = dialogueBuilder.create();
+        //Set window parameters to wrap content
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        dialog.show();
+        dialog.getWindow().setAttributes(lp);
+        //startActivity(new Intent(HospitalInteract.this, hrBed.class));;
 
     }
 }
