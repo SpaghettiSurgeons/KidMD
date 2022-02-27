2f1fad5f6e7b476740224d792fb12cabb4ee1ed4
Nabiha Bashir <bashirnabiha@gmail.com>
2022-02-26 18:45:39 -0800

    Created Appendectomy Page 2

---
+++ app/src/main/java/com/example/kidmd/prAppendectomy2.java
@@ -0,0 +1,14 @@
+package com.example.kidmd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class prAppendectomy2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pr_appendectomy2);
+    }
+}
