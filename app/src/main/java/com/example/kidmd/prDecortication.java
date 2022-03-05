b20713a14cb658f367124da204d423eac5f84758
Nabiha Bashir <bashirnabiha@gmail.com>
2022-03-05 15:11:25 -0800

    added second page to Decortication

--- app/src/main/java/com/example/kidmd/prDecortication.java
+++ app/src/main/java/com/example/kidmd/prDecortication.java
@@ -2,13 +2,33 @@ package com.example.kidmd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class prDecortication extends AppCompatActivity {
+
+    private ImageButton pageForward;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pr_decortication);
+
+
+        //---page forward button---
+        pageForward = (ImageButton) findViewById(R.id.decPage1Next);
+        pageForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(prDecortication.this, prDecortication2.class));
+            }
+        });
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/kidmd/prDecortication2.java
@@ -0,0 +1,30 @@
+package com.example.kidmd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class prDecortication2 extends AppCompatActivity {
+
+
+    private ImageButton pageBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pr_decortication2);
+
+        //---page back button---
+        pageBack = (ImageButton) findViewById(R.id.decorPage2Back);
+        pageBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(prDecortication2.this, prDecortication.class));
+            }
+        });
+    }
+
+}
