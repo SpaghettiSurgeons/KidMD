de2d996b7eb32590087c9905deb27dba4698e249
Nabiha Bashir <bashirnabiha@gmail.com>
2022-04-23 04:05:52 -0700

    fixed Hyperinsulinism pages

--- app/src/main/java/com/example/kidmd/prHyperinsulinism.java
+++ app/src/main/java/com/example/kidmd/prHyperinsulinism.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class prHyperinsulinism extends AppCompatActivity implements View.OnClickListener {
 
@@ -14,6 +19,11 @@ public class prHyperinsulinism extends AppCompatActivity implements View.OnClick
     private ImageButton xBack;
     // Bottom Toolbar
     private AppCompatImageView home_button, explore_button, notifications_button, profile_button;
+
+    //for audio
+    ImageButton hyperAudio;
+    TextView hyperDesc;
+    TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +57,32 @@ public class prHyperinsulinism extends AppCompatActivity implements View.OnClick
                 startActivity(new Intent(prHyperinsulinism.this, ProceduresList.class));
             }
         });
+
+        //audio
+        hyperAudio = findViewById(R.id.hyperAudio);
+        hyperDesc = findViewById(R.id.prHyperDesc);
+
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if (i!= TextToSpeech.ERROR){
+                    //set language to US
+                    textToSpeech.setLanguage(Locale.US); }
+            }
+        });
+        hyperAudio.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                textToSpeech.speak(hyperDesc.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
+        //change font
+        Typeface baloo = Typeface.createFromAsset(getAssets(), "fonts/Baloo-Regular.ttf");
+        ((TextView) findViewById(R.id.Hyperinsulinism)).setTypeface(baloo);
+        ((TextView) findViewById(R.id.Hyperinsulinism2)).setTypeface(baloo);
+        ((TextView) findViewById(R.id.prHyperTitle)).setTypeface(baloo);
+        (hyperDesc).setTypeface(baloo);
     }
 
     public void onClick(View view) {
--- app/src/main/java/com/example/kidmd/prHyperinsulinism2.java
+++ app/src/main/java/com/example/kidmd/prHyperinsulinism2.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class prHyperinsulinism2 extends AppCompatActivity implements View.OnClickListener {
 
@@ -14,6 +19,11 @@ public class prHyperinsulinism2 extends AppCompatActivity implements View.OnClic
     private ImageButton xBack;
     // Bottom Toolbar
     private AppCompatImageView home_button, explore_button, notifications_button, profile_button;
+
+    //for audio
+    ImageButton hyperAudio2;
+    TextView hyperDesc2;
+    TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +57,32 @@ public class prHyperinsulinism2 extends AppCompatActivity implements View.OnClic
                 startActivity(new Intent(prHyperinsulinism2.this, ProceduresList.class));
             }
         });
+
+        //audio
+        hyperAudio2 = findViewById(R.id.hyperAudio2);
+        hyperDesc2 = findViewById(R.id.prHyperDesc2);
+
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if (i!= TextToSpeech.ERROR){
+                    //set language to US
+                    textToSpeech.setLanguage(Locale.US); }
+            }
+        });
+        hyperAudio2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                textToSpeech.speak(hyperDesc2.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
+        //change font
+        Typeface baloo = Typeface.createFromAsset(getAssets(), "fonts/Baloo-Regular.ttf");
+        ((TextView) findViewById(R.id.Hyperinsulinism_2)).setTypeface(baloo);
+        ((TextView) findViewById(R.id.Hyperinsulinism2_2)).setTypeface(baloo);
+        ((TextView) findViewById(R.id.prHyperTitle2)).setTypeface(baloo);
+        (hyperDesc2).setTypeface(baloo);
     }
 
     public void onClick(View view) {
