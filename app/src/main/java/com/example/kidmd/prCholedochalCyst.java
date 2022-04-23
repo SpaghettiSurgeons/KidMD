15b8acea530e304f1aa3e71d444beab2739df459
Nabiha Bashir <bashirnabiha@gmail.com>
2022-04-23 00:34:08 -0700

    fixed Choledochal Cyst pages

--- app/src/main/java/com/example/kidmd/prCholedochalCyst.java
+++ app/src/main/java/com/example/kidmd/prCholedochalCyst.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class prCholedochalCyst extends AppCompatActivity implements View.OnClickListener {
 
@@ -14,6 +19,11 @@ public class prCholedochalCyst extends AppCompatActivity implements View.OnClick
     private ImageButton xBack;
     // Bottom Toolbar
     private AppCompatImageView home_button, explore_button, notifications_button, profile_button;
+
+    //for audio
+    ImageButton cholCystAudio;
+    TextView cholCystDesc;
+    TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +57,32 @@ public class prCholedochalCyst extends AppCompatActivity implements View.OnClick
                 startActivity(new Intent(prCholedochalCyst.this, ProceduresList.class));
             }
         });
+
+        //audio
+        cholCystAudio = findViewById(R.id.cholCystAudio);
+        cholCystDesc = findViewById(R.id.prCholCystDesc);
+
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if (i!= TextToSpeech.ERROR){
+                    //set language to US
+                    textToSpeech.setLanguage(Locale.US); }
+            }
+        });
+        cholCystAudio.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                textToSpeech.speak(cholCystDesc.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
+        //change font
+        Typeface baloo = Typeface.createFromAsset(getAssets(), "fonts/Baloo-Regular.ttf");
+        ((TextView) findViewById(R.id.choledochalCyst)).setTypeface(baloo);
+        ((TextView) findViewById(R.id.prCholCystTitle)).setTypeface(baloo);
+        (cholCystDesc).setTypeface(baloo);
+
     }
     public void onClick(View view) {
         switch (view.getId()) {
--- app/src/main/java/com/example/kidmd/prCholedochalCyst2.java
+++ app/src/main/java/com/example/kidmd/prCholedochalCyst2.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class prCholedochalCyst2 extends AppCompatActivity implements View.OnClickListener {
 
@@ -14,6 +19,11 @@ public class prCholedochalCyst2 extends AppCompatActivity implements View.OnClic
     private ImageButton xBack;
     // Bottom Toolbar
     private AppCompatImageView home_button, explore_button, notifications_button, profile_button;
+
+    //for audio
+    ImageButton cholCystAudio2;
+    TextView cholCystDesc2;
+    TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +57,32 @@ public class prCholedochalCyst2 extends AppCompatActivity implements View.OnClic
                 startActivity(new Intent(prCholedochalCyst2.this, ProceduresList.class));
             }
         });
+
+        //audio
+        cholCystAudio2 = findViewById(R.id.cholCystAudio2);
+        cholCystDesc2 = findViewById(R.id.prCholCystDesc2);
+
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if (i!= TextToSpeech.ERROR){
+                    //set language to US
+                    textToSpeech.setLanguage(Locale.US); }
+            }
+        });
+        cholCystAudio2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                textToSpeech.speak(cholCystDesc2.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
+        //change font
+        Typeface baloo = Typeface.createFromAsset(getAssets(), "fonts/Baloo-Regular.ttf");
+        ((TextView) findViewById(R.id.choledochalCyst2)).setTypeface(baloo);
+        ((TextView) findViewById(R.id.prCholCystTitle2)).setTypeface(baloo);
+        (cholCystDesc2).setTypeface(baloo);
+
     }
     public void onClick(View view) {
         switch (view.getId()) {
