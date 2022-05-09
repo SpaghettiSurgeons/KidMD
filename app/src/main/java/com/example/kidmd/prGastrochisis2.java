8dadffd5bf42b821a334ecbe9bec6b9bc41757e0
Nabiha Bashir <bashirnabiha@gmail.com>
2022-05-08 22:24:14 -0700

    added Gastrochisis procedure

---
+++ app/src/main/java/com/example/kidmd/prGastrochisis2.java
@@ -0,0 +1,14 @@
+package com.example.kidmd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class prGastrochisis2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pr_gastrochisis2);
+    }
+}
